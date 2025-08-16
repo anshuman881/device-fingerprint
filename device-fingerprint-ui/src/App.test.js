94ca2ad384faf81a611993aeec29081a25775b4d
anshuman881 <rajputanshuman95@outlook.com>
2025-08-16 21:14:47 +0530

    Deleting extra file

--- device-fingerprint-ui/src/App.test.js
+++ device-fingerprint-ui/src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('random test', () => {
+  const value = 1 + 1;
+  expect(value).toBe(2);
 });
